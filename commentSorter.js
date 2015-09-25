b1a6d5311ee77ae668dc2ee2d2e38ffaa268c267
GMeyr <gabrielmeyr@gmail.com>
2015-09-25 13:06:02 -0400

    Create commentSorter.js

---
+++ commentSorter.js
@@ -0,0 +1,60 @@
+
+function commentSort (arr) {
+  arr.forEach(function () {
+    var current_type = calcCommType (obj);
+    obj.type = current_type;
+    
+    var statcomms = [],
+        ratcomms = [],
+        moralcomms = [],
+        aneccomms = [];
+        
+    switch (obj.type) {
+      case "statistical":
+        statcomms.push(obj);
+        break;
+      case "rational":
+        ratcomms.push(obj);
+        break;
+      case "moral":
+        moralcomms.push(obj);
+        break;
+      case "anecdotal":
+        aneccomms.push(obj);
+        break;
+      default:
+        console.log("--commentSort failed to identify obj.type");
+    }
+  });
+}
+
+function calcCommType (obj){
+    var winner = "none";
+    var objArr =  [
+ 		 {name: "statvotes",
+ 		  num: obj.statvotes},
+ 	         {name: "ratvotes",
+ 	          num: obj.ratvotes},
+ 		 {name: "moralvotes",
+ 		  num: obj.moralvotes},
+ 		 {name: "anecvotes",
+ 		  num: obj.anecvotes},
+ 		 {name: "none",
+ 		  num: 0}];
+
+    objArr.forEach(function(x){
+        console.log(x.name + " is being compared to " + obj[winner]);
+        console.log(x.num + " is being compared to " + winner.num);
+        if(x.num > exdata[winner]){
+           console.log(x.name + "is greater than" + obj.winner);
+           winner = x.name
+        }
+    })
+        //badvote threshold
+        if(obj.badvotes < 3)
+          return winner;
+        else return "badvotes";
+}
+
+
+exports.commentSort = commentSort;
